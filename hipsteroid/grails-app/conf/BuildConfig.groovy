ee3703430a7c0080b32b83587facec64365c54ea
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-11-19 22:35:12 +0000

    empty grails app

---
+++ hipsteroid/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,40 @@
+grails.servlet.version = '3.0'
+grails.project.work.dir = 'target'
+grails.project.class.dir = 'target/classes'
+grails.project.test.class.dir = 'target/test-classes'
+grails.project.test.reports.dir = 'target/test-reports'
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
+
+grails.project.dependency.resolution = {
+
+    inherits 'global'
+	log 'error'
+    checksums true
+
+    repositories {
+        inherits true
+
+		mavenLocal()
+
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        mavenCentral()
+    }
+
+    dependencies {
+
+    }
+
+    plugins {
+        runtime ':jquery:1.8.3'
+        runtime ':resources:1.2.RC2'
+        runtime ':zipped-resources:1.0'
+        runtime ':cached-resources:1.0'
+        runtime ':yui-minify-resources:0.1.5'
+
+        build ":tomcat:$grailsVersion"
+    }
+}
