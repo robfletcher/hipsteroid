6c448e3642969f68157cf9d2ae7c0d28edf4dc65
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-11-22 22:58:00 +0000

    auto-create some pictures on startup

--- hipsteroid/grails-app/conf/BootStrap.groovy
+++ hipsteroid/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,20 @@
+import co.freeside.hipsteroid.*
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+
+		if (Picture.count() == 0) {
+
+			['gibson', 'manhattan', 'martini', 'oldfashioned'].each {
+				def image = new File(BootStrap.getResource("/${it}.jpg").toURI())
+				new Picture(image: image.bytes, uploadedBy: 61233112).save(failOnError: true)
+			}
+
+		}
+
+	}
+
+	def destroy = {
+	}
 }
