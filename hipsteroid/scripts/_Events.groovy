3a7c70efe7b7d0b1211cbf544e816e0e066d1792
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-12-14 15:09:27 +0000

    automatically run grunt when grails compiles

--- hipsteroid/scripts/_Events.groovy
+++ hipsteroid/scripts/_Events.groovy
@@ -1,5 +1,10 @@
 eventCompileStart = {
+
 	ant.copy(todir: resourcesDirPath) {
 		fileset dir: 'test/resources'
 	}
+
+	event 'StatusUpdate', ['compiling front-end resources']
+	ant.exec executable: 'grunt', failonerror: true
+
 }
