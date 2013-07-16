212493d73179ac04ec43e658365804ff1a9116f8
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-07-16 22:33:22 +0100

    upgrade embedded vert.x version

--- hipsteroid/grails-app/conf/BuildConfig.groovy
+++ hipsteroid/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 
-		compile 'org.vert-x:vertx-lang-groovy:1.3.0.final',
+		compile 'org.vert-x:vertx-lang-groovy:1.3.1.final',
 				'org.im4java:im4java:1.2.0',
 				'com.github.mfornos:humanize-slim:0.1.4'
 
