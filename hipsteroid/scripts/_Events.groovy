8450f95d7c37eadf8467fd03741caa48f5b6b79c
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-07-16 21:44:40 +0100

    upgrade Grunt components

--- hipsteroid/scripts/_Events.groovy
+++ hipsteroid/scripts/_Events.groovy
@@ -4,7 +4,20 @@ eventCompileStart = {
 		fileset dir: 'test/resources'
 	}
 
-	event 'StatusUpdate', ['compiling front-end resources']
-	ant.exec executable: 'grunt', failonerror: false
+	event 'StatusUpdate', ['Compiling front-end resources']
+	def process = 'grunt'.execute()
+	def returnCode = process.waitFor()
+	switch (returnCode) {
+		case 0:
+			event 'StatusUpdate', ['Compiled front-end resources']
+			break
+		case 2:
+			event 'StatusError', ['Grunt is not installed. Front-end resources were NOT compiled.']
+			break
+		default:
+			event 'StatusError', ["Failed to compile front-end resources: ${process.errorStream.text}"]
+	}
+
+
 
 }
